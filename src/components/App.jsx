90e0df7ed2f6e6123e5e1bc9d39846a9f586ea79
katscap <katielaw97@gmail.com>
2021-09-21 22:59:00 -0400

    started auth routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {useState} from 'react';
+import { Route } from 'react-router-dom';
+import DummyLoggedIn from './DummyLoggedIn.js';
 
 const App = function() {
   return (
     <div>
       <h1>Welcome to our Application!</h1>
+      <Route exact path="/dummyloggedin" component={DummyLoggedIn} />
     </div>
   );
-}
+};
 
 export default App;
