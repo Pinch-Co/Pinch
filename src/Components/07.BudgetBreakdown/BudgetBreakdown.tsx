7b3781a70cd11c9c6fce7fdcdc62869c039142a6
Seiji Matsumoto <seijim27@gmail.com>
2021-11-02 11:57:57 -0400

    Fixed styling for buttons and mobile

--- src/Components/07.BudgetBreakdown/BudgetBreakdown.tsx
+++ src/Components/07.BudgetBreakdown/BudgetBreakdown.tsx
@@ -242,7 +242,7 @@ function BudgetBreakdown() {
                       // eslint-disable-next-line react/no-array-index-key
                       <div className="bb-add-custom-row" key={row + i} id={`bb-edit-row-${i}`}>
                         <label className="bb-add-input-group" htmlFor="bb-add-input-income">
-                          <div className="bb-input-title">Budget Name</div>
+                          <div className="bb-input-title">Expense Name</div>
                           <div className="bb-input-box">
                             <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" />
                           </div>
@@ -254,7 +254,7 @@ function BudgetBreakdown() {
                             <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" />
                           </div>
                         </label>
-                        {i > 0 ? <button type="button" className="bb-delete-custom-btn" onClick={(e: any) => deleteExpense(e, i)}>Delete</button> : null}
+                        {i > 0 ? <button type="button" className="bb-delete-custom-btn" onClick={(e: any) => deleteExpense(e, i)}><img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/trashcan-512.png" alt="delete icon" className="bb-delete-icon" /></button> : null}
                       </div>
                     ))}
                     <input type="submit" onClick={(e: any) => setNewBudget(e)} className="bb-submit-custom-btn" value="Submit" />
@@ -278,181 +278,5 @@ function BudgetBreakdown() {
     </div>
   );
 }
-
-// return (
-//   <div className="bb-container">
-//     <div className="bb-mid-div">
-//       <div className="bb-left">
-//         <div className="bb-tabs">
-//           {budgets ? budgets.map((budget: any, i: number) => (
-//             <button key={budget.id} id={`bb-btn-${i}`} type="button" className="bb-tab" onClick={() => changeTabs(i)}>
-//               Budget
-//               {' '}
-//               {i + 1}
-//             </button>
-//           )) : null}
-//           <button type="button" className="bb-tab" id="bb-add-btn" onClick={changeToAdd}>Add +</button>
-//         </div>
-//         <div className="bb-budget-box">
-//           {!showAdd ? (
-//             <div className="bb-budget-top">
-//               <>
-//                 <div className="bb-budget-top-left">
-//                   {activeBudget.map((row: any) => {
-//                     if (row[0] === 'Income') {
-//                       return (
-//                         <div className="bb-income" key={row[1]}>
-//                           Income:
-//                           {' '}
-//                           {row[1]}
-//                           /mo
-//                         </div>
-//                       );
-//                     }
-//                     return null;
-//                   })}
-//                   <select onChange={(e: any) => sortExpenses(e)} className="bb-sort" name="sort" id="bb-sort-select">
-//                     <option defaultValue="Sort">Sort</option>
-//                     <option value="price-highest">Price Highest to Lowest</option>
-//                     <option value="price-lowest">Price Lowest to Highest</option>
-//                     <option value="alphabetical-AtoZ">Alphabetical A to Z</option>
-//                     <option value="alphabetical-ZtoA">Alphabetical Z to A</option>
-//                   </select>
-//                 </div>
-//                 {!editActive ? <button type="button" className="bb-budget-top-right" onClick={() => setEditActive(true)}>Edit</button> : <button type="button" className="bb-budget-top-right" onClick={() => setEditActive(false)}>Cancel</button>}
-//               </>
-//             </div>
-//           ) : null}
-//           <div className="bb-budget-middle">
-//             {!showAdd ? (
-//               <>
-//                 {!editActive ? (
-//                   <>
-//                     {activeBudget.map((row: any, i: number) => {
-//                       if (i > 0) {
-//                         return (
-//                           <div key={row[i]} className="bb-budget-middle-row">
-//                             <ul>
-//                               <li className="bb-budget-category">
-//                                 {row[0]}
-//                               </li>
-//                             </ul>
-//                             <div className="bb-budget-dollars">{row[1]}</div>
-//                           </div>
-//                         );
-//                       }
-//                       return null;
-//                     })}
-//                     <div className="bb-budget-bottom">
-//                       <div className="bb-budget-total">
-//                         <div className="bb-budget-total-title">Total:</div>
-//                         <div className="bb-budget-total-amount>">
-//                           $
-//                           {activeTotal}
-//                         </div>
-//                       </div>
-//                       <div className="bb-budget-difference">
-//                         <div className="bb-budget-total-title">Remaining:</div>
-//                         <div className="bb-budget-total-amount>">
-//                           {activeBudget.map((row: any) => {
-//                             if (row[0] === 'Income') {
-//                               return (
-//                                 <div className="bb-income" key={row[1]}>
-//                                   $
-//                                   {parseInt(row[1].slice(1), 10) - activeTotal}
-//                                 </div>
-//                               );
-//                             }
-//                             return null;
-//                           })}
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </>
-//                 ) : (
-//                   <div className="bb-edit-form">
-//                     <form id="bb-form-edit" onSubmit={(e: any) => editActiveBudget(e)}>
-//                       {activeBudget.map((row: any, i: number) => {
-//                         if (i > 0) {
-//                           return (
-//                             // eslint-disable-next-line react/no-array-index-key
-//                             <div className="bb-add-custom-row" key={row[0] + i}>
-//                               <label htmlFor="bb-add-input-income" className="bb-add-input-group">
-//                                 <div className="bb-input-title">Name</div>
-//                                 <div className="bb-input-box">
-//                                   <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" defaultValue={row[0]} />
-//                                 </div>
-//                               </label>
-//                               <label htmlFor="bb-add-input-income" className="bb-add-input-group">
-//                                 <div className="bb-input-title">Amount</div>
-//                                 <div className="bb-input-box">
-//                                   <span className="bb-prefix">$</span>
-//                                   <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" defaultValue={row[1].slice(1)} />
-//                                 </div>
-//                               </label>
-//                             </div>
-//                           );
-//                         }
-//                         return null;
-//                       })}
-//                       <input type="submit" onClick={(e: any) => editActiveBudget(e)} className="bb-submit-custom-btn" value="Submit" />
-//                     </form>
-//                   </div>
-//                 )}
-//               </>
-//             ) : (
-//               <div className="bb-add-body">
-//                 <h1 className="bb-add-title">Create a New Budget</h1>
-//                 <form className="bb-add-form" id="myForm" onSubmit={(e: any) => addNewBudget(e)}>
-//                   <label htmlFor="bb-add-input-income" className="bb-add-input-group">
-//                     <div className="bb-input-title">Income (monthly)*</div>
-//                     <div className="bb-input-box">
-//                       <span className="bb-prefix">$</span>
-//                       <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" />
-//                     </div>
-//                   </label>
-//                   <div className="bb-add-custom">
-//                     {numberOfExpenses.map((each: string, i: number) => (
-//                       <div className="bb-add-custom-row" id={`custom-row-${i}`}>
-//                         <label htmlFor="bb-add-input-custom" className="bb-add-input-group">
-//                           <div className="bb-input-title">Name</div>
-//                           <div className="bb-input-box">
-//                             <input type="text" id="bb-add-input-income" className="bb-input-field" autoComplete="off" placeholder="Category name..." />
-//                           </div>
-//                         </label>
-//                         <label htmlFor="bb-add-input-custom" className="bb-add-input-group">
-//                           <div className="bb-input-title">Amount</div>
-//                           <div className="bb-input-box">
-//                             <span className="bb-prefix">$</span>
-//                             <input type="text" id="bb-add-input-custom" className="bb-input-field" autoComplete="off" />
-//                           </div>
-//                         </label>
-//                         <button type="button" className="bb-delete-custom-btn" onClick={(e: any) => deleteExpense(e, i)}>X</button>
-//                       </div>
-//                     ))}
-//                   </div>
-//                   <div className="bb-buttons-div">
-//                     <button type="button" className="bb-add-custom-btn" onClick={addExpense}>Add an expense +</button>
-//                     <input type="submit" className="bb-submit-custom-btn" value="Submit" />
-//                   </div>
-//                 </form>
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//       <div className="bb-right">
-//         <div className="bb-chart">
-//           <img src="https://upload.wikimedia.org/wikipedia/commons/5/54/Turkish_general_election%2C_2007_pie_chart.png" alt="pie chart" className="bb-pie-chart" />
-//         </div>
-//         <select className="bb-chart-type">
-//           <option value="Pie Chart">Pie Chart</option>
-//           <option value="Line Graph">Line Graph</option>
-//           <option value="Bar Graph">Bar Graph</option>
-//         </select>
-//       </div>
-//     </div>
-//   </div>
-// );
 
 export default BudgetBreakdown;
